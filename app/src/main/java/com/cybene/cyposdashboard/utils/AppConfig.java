b83170f43e257a657ef0acd12cefb2c2a98ecb51
Theonlysmartboy <o2jose43@gmail.com>
2025-10-13 16:37:53 +0300

    Added salesman items display

--- app/src/main/java/com/cybene/cyposdashboard/utils/AppConfig.java
+++ app/src/main/java/com/cybene/cyposdashboard/utils/AppConfig.java
@@ -3,13 +3,11 @@ package com.cybene.cyposdashboard.utils;
 public class AppConfig {
 
     public static final String BASE_URL = AppController.getUrlPath() ;
-
     public static final String URL_REGISTER = BASE_URL+"/cyposbackend/controller/auth/RegisterController.php";
     public static final String URL_LOGIN = BASE_URL+"/cyposbackend/controller/auth/LoginController.php";
     public static final String URL_PASSWORD_RESET= BASE_URL+"/cyposbackend/controller/auth/PasswordResetController.php";
     public static final String URL_RECOVER_PASSWORD= BASE_URL+"/cyposbackend/controller/auth/LoginController.php";
     public static final String URL_DASHBOARD = BASE_URL+"/cyposbackend/controller/dashboard/DashboardController.php";
-
     public static final String URL_INVOICE_LIST = BASE_URL+"/cyposbackend/controller/sales/InvoiceListController.php";
     public static final String URL_TILL_CASH_PICKUP = BASE_URL+"/cyposbackend/controller/sales/TillCashPickUpController.php";
     public static final String URL_CASH_SALE_MODE = BASE_URL+"/cyposbackend/controller/sales/CashSalePaymentModeController.php";
@@ -19,7 +17,6 @@ public class AppConfig {
     public static final String URL_TRANSACTION_WISE_SALE = BASE_URL+"/cyposbackend/controller/sales/TransactionWiseSalesDataGridController.php";
     public static final String URL_SALESMAN_WISE_SALE = BASE_URL+"/cyposbackend/controller/sales/SalesManWiseSalesDataGridController.php";
     public static final String URL_CUSTOMER_WISE_SALE = BASE_URL+"/cyposbackend/controller/sales/CustomerWiseSalesDataGridController.php";
-
     public static final String URL_DAILY_PURCHASE = BASE_URL+"/cyposbackend/controller/purchases/DailyPurchaseDataGridController.php";
     public static final String URL_MONTHLY_PURCHASE = BASE_URL+"/cyposbackend/controller/purchases/MonthlyPurchaseDataGridController.php";
     public static final String URL_GRN_LIST = BASE_URL+"/cyposbackend/controller/purchases/GrnListController.php";
@@ -27,11 +24,9 @@ public class AppConfig {
     public static final String URL_LPO_LIST = BASE_URL+"/cyposbackend/controller/purchases/LpoListController.php";
     public static final String URL_SWP_LIST = BASE_URL+"/cyposbackend/controller/purchases/SupplierWisePurchaseListController.php";
     public static final String URL_DWP_LIST = BASE_URL+"/cyposbackend/controller/purchases/DepartmentWisePurchaseListController.php";
-
     public static final String URL_DNOTE_LIST = BASE_URL+"/cyposbackend/controller/inventories/DeliveryNoteListController.php";
     public static final String URL_DEPT_WISE_STOCK = BASE_URL+"/cyposbackend/controller/inventories/DepartmentWiseStockValuationController.php";
     public static final String URL_TOTAL_STOCK = BASE_URL+"/cyposbackend/controller/inventories/TotalStockValuationController.php";
-
     public static final String URL_CUSTOMER_OUTSTANDING = BASE_URL+"/cyposbackend/controller/accounts/CustomerOutstandingController.php";
     public static final String URL_SUPPLIER_OUTSTANDING = BASE_URL+"/cyposbackend/controller/accounts/SupplierOutstandingController.php";
     public static final String URL_RECEIPT_LIST = BASE_URL+"/cyposbackend/controller/accounts/ReceiptListController.php";
@@ -42,7 +37,6 @@ public class AppConfig {
     public static final String URL_POST_DATED_CHEQUE_SUPPLIER_LIST = BASE_URL+"/cyposbackend/controller/accounts/PostDatedChequeSupplierListController.php";
     public static final String URL_POST_DATED_CREDIT_NOTE_PURCHASE_LIST = BASE_URL+"/cyposbackend/controller/accounts/PostCreditNotePurchaseListController.php";
     public static final String URL_POST_DATED_CREDIT_NOTE_SALES_LIST = BASE_URL+"/cyposbackend/controller/accounts/PostCreditNoteSalesListController.php";
-
     public static final String URL_PAYMENT_LIST_2 = BASE_URL+"/cyposbackend/controller/branches/BranchPaymentListController.php";
     public static final String URL_PAYMENT_REVERSAL_LIST_2 = BASE_URL+"/cyposbackend/controller/branches/BranchPaymentReversalListController.php";
     public static final String URL_RECEIPT_LIST_2 = BASE_URL+"/cyposbackend/controller/branches/BranchReceiptListController.php";
@@ -79,6 +73,7 @@ public class AppConfig {
     public static final String URL_SUPPLIER_WISE_MONTHLY_PURCHASE = BASE_URL+"/cyposbackend/controller/suppliers/MonthlyPurchaseSupplierWiseController.php";
     public static final String URL_SUPPLIER_LAST_PAYMENT_DETAILS = BASE_URL+"/cyposbackend/controller/suppliers/LastPaymentDetailsController.php";
     public static final String URL_SUPPLIER_CREDIT_DETAILS = BASE_URL+"/cyposbackend/controller/suppliers/SupplierCreditDetailsController.php";
+    public static final String URL_SALESMAN_ITEMS = BASE_URL+"/cyposbackend/controller/salesman/ItemsController.php";
     public static String loaderPrimaryColor = "#336699";
     public static String loaderSecondaryColor = "#003366";
     public static int loaderDotsCount = 10;
